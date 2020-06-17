7564d7e5a5eb7575ae54fafb7e3f4c32636e3c6a
Conor Barnes <conorbarnes93@gmail.com>
2020-06-17 15:29:04 -0700

    Add potential linecheck gamewin function

--- index.js
+++ index.js
@@ -41,7 +41,20 @@ let gameboard = [{
 // - blanks, frame
 console.log(gameboard[2])
 
-// if all 
+function linecheck() {
+    let line = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    for (let i = 0; i < line.length; i++) {
+        let check = []
+        for (let j = 0; j < 3; j++) {
+            if (diag[i][j] != null) {
+                check.push(diag[i][j])
+            }
+        }
+        if (check[0] === check[1] === check[2]) {
+            winner(latestplayer)
+        }
+    }
+}
 
 
 const playerFactory = (name) => {
