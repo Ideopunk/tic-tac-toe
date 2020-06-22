df4186b242a173e046637103fa7e8a193a836b20
Conor Barnes <conorbarnes93@gmail.com>
2020-06-22 08:03:40 -0700

    clean code, change gameboard from array of objects to simple array, begin work on DOM interaction

--- index.js
+++ index.js
@@ -1,60 +1,19 @@
 // module gameboard
-let gameboard = [{  
-        x: 0, 
-        y: 0,
-        value: ''      
-    },{
-        x: 1,
-        y: 0,
-        value: ''
-    },{
-        x: 2,
-        y:0,
-        value: ''
-    },{
-        x: 0,
-        y: 1,
-        value: ''
-    },{
-        x: 1,
-        y: 1,
-        value: ''
-    },{
-        x: 2,
-        y: 1,
-        value: ''
-    },{
-        x: 0,
-        y: 2,
-        value: ''
-    },{
-        x: 1,
-        y: 2,
-        value: ''
-    },{
-        x: 2,
-        y: 2,
-        value: ''
-    }
-];
-// - blanks, frame
-console.log(gameboard[2])
+let gameboard = ['', '', '', '', '', '', '', '', ''];
+
 
 // player templates
-const playerFactory = (name) => {
+const playerFactory = (name, symb) => {
     let score = 0;
     let turn = false;
-    return { name, score, turn };
+    return { name, score, turn, symb };
 };
 
 // create two players
-const player1 = playerFactory('Alice')
-const player2 = playerFactory('Bob')
+const player1 = playerFactory('Alice', 'x')
+const player2 = playerFactory('Bob', 'o')
 
-console.log(player1.name)
-console.log(player1.score)
-console.log(player1.turn)
-
+// all the things that happen in the game
 const gameflow = (function(player1, player2, gameboard){
     
     // Initialize game. 
@@ -64,8 +23,10 @@ const gameflow = (function(player1, player2, gameboard){
         let x = Math.random();
         if (x < 0.5) {
             player1.turn = true;
+            console.log(player1.name + ` goes first!`)
         } else {
             player2.turn = true;
+            console.log(player2.name + ` goes first!`)
         }
     }
 
@@ -78,25 +39,38 @@ const gameflow = (function(player1, player2, gameboard){
     }
 
     // see if there's a winner
-    const linecheck = () => {
-        console.log('linecheck')
-        let line = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-        let totalFilled = 0;
-        for (let i = 0; i < line.length; i++) {
-            let check = []
-            for (let j = 0; j < 3; j++) {
-                if (gameboard[i][j].value != null) {
-                    check.push(gameboard[i][j].value)
-                    totalFilled++;
-                }
-            }
-            if (check[0] === check[1] === check[2]) {
-                winner(latestplayer);
-            }
-        }
-        if (totalFilled === 24) {
-            tie();
-        }
+    // const linecheck = () => {
+    //     console.log('linecheck')
+    //     let line = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    //     let totalFilled = 0;
+    //     for (let i = 0; i < line.length; i++) {
+    //         let check = []
+    //         for (let j = 0; j < 3; j++) {
+    //             console.log(gameboard[i][j])
+    //             if (gameboard[i][j].value != null) {
+    //                 check.push(gameboard[i][j].value)
+    //                 totalFilled++;
+    //             }
+    //         }
+    //         if (check[0] === check[1] === check[2]) {
+    //             winner(latestplayer);
+    //         }
+    //     }
+    //     if (totalFilled === 24) {
+    //         tie();
+    //     }
+    // }
+
+    const gameMove = (player) => {
+        let a = player.symb;
+        let ttts = document.querySelectorAll(".cell")
+        ttts.forEach(ttt => {
+            // remove old event listeners?
+            ttt.addEventListener('click', () => {
+                console.log(ttt.childNodes)
+                // 
+            })
+        })
     }
 
     // turn control over to other player
@@ -105,9 +79,11 @@ const gameflow = (function(player1, player2, gameboard){
         if (player1.turn === true) {
             player1.turn = false;
             player2.turn = true;
+            gameMove(player2)
         } else {
             player1.turn = true;
             player2.turn = false;
+            gameMove(player1)
         }
     }
 
@@ -115,8 +91,7 @@ const gameflow = (function(player1, player2, gameboard){
     const turn = () => {
         console.log('turn')
         console.log(player1, player2, gameboard)
-        // playmove
-        linecheck;
+        // linecheck();
         newTurn(player1, player2, gameboard);
     }
     return {gamestart, turn}
